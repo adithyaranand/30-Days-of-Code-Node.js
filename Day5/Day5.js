464f08948cc879c57dd59a95a5d016ed1ad08c89
Adithya R Anand <94031516+adithyaranand@users.noreply.github.com>
2024-02-07 10:42:34 +0530

    Uploaded Day4 and Day5

---
+++ Day5/Day5.js
@@ -0,0 +1,12 @@
+const path = require('path');
+function checkFileExtension(filePath, expectedExtension) {
+    const fileExtension = path.extname(filePath);
+    if (fileExtension === expectedExtension) {
+        console.log(`File has the expected extension: ${expectedExtension}`);
+    } else {
+        console.log(`File does not have the expected extension. Expected: ${expectedExtension}, Actual: ${fileExtension}`);
+    }
+}
+// Test cases
+checkFileExtension('test-files/file1.txt', '.txt');
+checkFileExtension('test-files/image.png', '.jpg');
