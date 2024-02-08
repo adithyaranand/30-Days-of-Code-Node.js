e484228b5bddf53c04ed07576e47031ea34eb946
Adithya R Anand <adithyranand@gmail.com>
2024-02-08 12:04:01 +0530

    Day7 Added

---
+++ Day7/Day7.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+function requestLoggerMiddleware(req, res, next) {
+  const timestamp = new Date().toISOString();
+  const method = req.method;
+  console.log(`${timestamp} - ${method} request received.`);
+  next();
+}
+app.use(requestLoggerMiddleware);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
